30593683812193c8dc781bf046c39d5b2a25898f
Arijan <arijannp2005@gmail.com>
2021-11-01 18:09:28 +0100

    more table

--- src/classComponents/Table.jsx
+++ src/classComponents/Table.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react/cjs/react.development";
 import "../App.css";
 function Table({ columns, dataSource }) {
-  const dataCol = columns;
-  const tableData = dataSource;
+  const [dataCol, setDataCol] = useState(columns);
+  const [tableData, setTableData] = useState(dataSource);
+  function rowDelete(id) {
+    
+    let [...newTable] = tableData
+    newTable.splice(id,1)
+    setTableData(newTable);
+  }
+
   return (
     <table className="forTable">
       <tr className="forTd">
@@ -10,11 +18,17 @@ function Table({ columns, dataSource }) {
         })}
       </tr>
       {tableData.map((value, index) => {
-        return <tr>
-        {dataCol.map((el) => { 
-          return  <td key={index}>{value[el.dataIndex]}</td>
-        })}
-        </tr>})}
+        return (
+          <tr>
+            {dataCol.map((el) => {
+              return <td key={index}>{value[el.dataIndex]}</td>;
+            })}
+            <td>
+              <button onClick={() => rowDelete(index)}>delete</button>
+            </td>
+          </tr>
+        );
+      })}
     </table>
   );
 }
